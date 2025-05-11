84aedf9cc0fcd4ceaa8e0a85c5e17d25e5862d03
saif ali <saifalinagori78@gmail.com>
2025-05-11 23:51:03 +0530

    Add file

--- server/config/cloudinary.config.js
+++ server/config/cloudinary.config.js
@@ -1,19 +1,11 @@
-
 // /backend/utils/cloudinary.js
-const cloudinary = require('cloudinary').v2;
+const cloudinary = require('cloudinary').v2;  
 
-<<<<<<< HEAD
-
-cloudinary.config({ 
-  cloud_name: process.env.CLOUDE_NAME,
-  api_key: process.env.CLOUDE_KEY,
-  api_secret: process.env.CLOUDE_SECRET , 
-=======
 cloudinary.config({
   cloud_name: process.env.CLOUDE_NAME,
   api_key: process.env.CLOUDE_KEY,
   api_secret: process.env.CLOUDE_SECRET, 
->>>>>>> 0d84b22c1acd5a7faa8dd261896e692aee1e4705
+
 });
 
 module.exports = cloudinary;
